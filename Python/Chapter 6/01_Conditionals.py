d8e0e18b20863ddb040a419680598a8c916d2a19
Karan Kumar <138513742+Kraken5651@users.noreply.github.com>
2025-11-18 11:02:49 +0530

    Conditional Statements

---
+++ Python/Chapter 6/01_Conditionals.py
@@ -0,0 +1,6 @@
+a = input("Enter True or False : ")
+# if else ladder
+if(a == "True"):
+    print("The user said TRUE")
+else:
+    print("Please Enter Only True or False.")
---
+++ Python/Chapter 6/02_if_elif_else_ladder.py
@@ -0,0 +1,14 @@
+a = input("Enter True or False : ")
+
+# if elif else ladder
+
+if(a == "True"):
+    print("The user said TRUE")
+elif(a == "False"):
+    print("The user said FALSE")
+elif(a == "true"):
+    print("The user said TRUE")
+elif(a == "false"):
+    print("The user said FALSE")
+else:
+    print("Please Enter Only True or False.")
