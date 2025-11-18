d964041dae1726f13b29f8418be1718b7a6b98a0
Karan Kumar <138513742+Kraken5651@users.noreply.github.com>
2025-11-19 01:44:43 +0530

    Loops Problem Statement

---
+++ Python/Chapter 7/Chapter 7 PS/02_problem2.py
@@ -0,0 +1,5 @@
+l = ["Surbhi", "Somya", "Samarth", "Harsh", "Rahul", "Raman"]
+
+for name in l:
+    if(name.startswith("S")):
+        print(f"Hello {name}")
---
+++ Python/Chapter 7/Chapter 7 PS/05_problem5.py
@@ -0,0 +1,8 @@
+i = 0
+n = 7
+sum = 0
+while(i<=n):
+    sum += i
+    i +=1
+    
+print(sum)
