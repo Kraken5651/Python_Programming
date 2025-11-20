a6538bde5b2ba7454aca872300be23a57feb16da
Karan Kumar <138513742+Kraken5651@users.noreply.github.com>
2025-11-21 00:30:07 +0530

    Functions

---
+++ Python/Chapter 8/03_function_with_arguments.py
@@ -0,0 +1,8 @@
+def goodDay(name, end):
+    print("Good Day, " + name)
+    print(end)
+    return 7
+    
+a = goodDay("Karan", "Bye")
+print(a)
+
---
+++ Python/Chapter 8/04_default_arguments.py
@@ -0,0 +1,5 @@
+def goodDay(name, end = "Thank you"):
+    print(f"Good Day, {name}")
+    print(end)
+    
+goodDay("Karan")
